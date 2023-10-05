e8afa4b51f74dfc6621c53b538930581cd44bdd4
Nikeshana <146467081+Nikeshana@users.noreply.github.com>
2023-10-05 10:37:27 +0530

    Update footer.php

---
+++ footer.php
@@ -0,0 +1,125 @@
+<!DOCTYPE html>
+
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <link rel="stylesheet" href="bootstrap.css" />
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.9.1/font/bootstrap-icons.css" />
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-alpha1/dist/css/bootstrap.min.css" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" />
+
+</head>
+
+<body>
+
+    <footer class="text-white pb-2 pt-2" style="background-color: #271140;">
+
+        <div class="col-11 text-md-start">
+
+            <div class="row text-sm-center text-md-start">
+
+                <div class="col-12 col-md-3 col-lg-4 mx-auto ">
+
+                    <h5 class="text-uppercase mt-4 fw-bold text-white text-uppercase">Migten</h5>
+                    <p class="text-white text-center">Here we CharityDonation.lk&trade; <br/>Your donation can make a world of difference. Join us in our mission to change lives and create a brighter future for those in need.</p>
+
+                    <br/>
+                    <br/>
+                    <br/>
+                    <hr class="text-white"/>
+
+                    <div class="col-12">
+                        <p class="text-white">Copyright &copy;2k23 Migten Rights Reserved</p>
+                    </div>
+                    <hr class="text-white"/>
+                </div>
+
+
+
+
+                <div class="col-12 col-md-3 col-lg-4 mx-auto mt-4">
+
+                    <h5 class="text-uppercase fw-bold text-white mb-4">Contact Us</h5>
+                    
+                    <p class="text-white"><i class="bi bi-house-fill text-white"></i> No.66 Amal Kandy Rd, Kalalpitiya, Ukuwela</p>
+                    <p class="text-white"><i class="bi bi-at text-white"></i> Migten@gmail.com</p>
+                    
+                        <p class="text-white"><i class="bi bi-telephone-fill text-white"></i> (+94)75 6985419</p>
+                    
+
+                    <hr class="text-white"/>
+
+                    <div class="col-12">
+                        <div class="text-center text-md-start">
+
+                            <ul class="list-unstyled list-inline">
+
+                                <li class="list-inline-item">
+
+                                    <a href="#" class="form-floating text-white">
+                                        <i class="bi bi-facebook" style="font-size: 22px;"></i>
+                                    </a>
+
+                                </li>
+
+                                <li class="list-inline-item">
+
+                                    <a href="#" class="form-floating text-white">
+                                        <i class="bi bi-twitter" style="font-size: 22px;"></i>
+                                    </a>
+
+                                </li>
+
+                                <li class="list-inline-item">
+
+                                    <a href="#" class="form-floating text-white">
+                                        <i class="bi bi-whatsapp" style="font-size: 22px;"></i>
+                                    </a>
+
+                                </li>
+
+                                <li class="list-inline-item">
+
+                                    <a href="#" class="form-floating text-white">
+                                        <i class="bi bi-linkedin" style="font-size: 22px;"></i>
+                                    </a>
+
+                                </li>
+
+                                <li class="list-inline-item">
+
+                                    <a href="#" class="form-floating text-white">
+                                        <i class="bi bi-youtube" style="font-size: 22px;"></i>
+                                    </a>
+
+                                </li>
+
+                            </ul>
+
+                        </div>
+                    </div>
+
+
+                </div>
+                
+
+                <!-- <hr style="background-color: #f13451; height: 10px;" /> -->
+
+            </div>
+            <!-- <div class="col12
+            ">
+
+            </div> -->
+
+        </div>
+
+    </footer>
+
+
+
+</body>
+
+</html>
