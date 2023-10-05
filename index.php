3827c77cbab41238f9b88f303e535c2ac9aa20ee
Nikeshana <146467081+Nikeshana@users.noreply.github.com>
2023-10-05 11:02:05 +0530

    header2

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Migten</title>
+</head>
+<body>
+<?php
+include "header.php"
+?>  
+<?php
+include "header2.php"
+?>  
+
+<?php
+include "footer.php"
+?>
+</body>
+</html>
